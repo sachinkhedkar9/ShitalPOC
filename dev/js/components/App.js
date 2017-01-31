dbdd23d45be891aef72bc130062e4c0283fb0052
Sachin Khedkar <sachink2@outlook.com>
2017-01-31 14:15:06 +0530

    css changes

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -27,18 +27,26 @@ class App extends Component {
       <div>
         <div className="col-lg-4 col-md-4 col-sm-3 col-xs-3"></div>
         <div className="html-page col-lg-4 col-md-4 col-sm-6 col-xs-6">
-          <div className={styles.loginPage}>
-            <h2>Login</h2>
-
-            <form>
+          <div className={styles.loginPage + "login-container sap-form"}>
+            <form class="form-signin content" name="loginForm">
+              <h2 class="form-signin-heading">Login</h2>
               <div className="login-form form-inline form-group" onSubmit={this.handleSubmit}>
-                <div className="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                  <input type="text" ref="username" placeholder="Enter a username"
-                    className="form-control" />
+                <div className="row bottom-margin">
+                  <label for="username" className="col-lg-3 col-md-3 col-sm-3 col-xs-12">User Name</label>
+                  <input type="text" ref="username" id="username" placeholder="User Name"
+                    required="" autofocus="" name="username"
+                    className="form-control col-lg-9 col-md-9 col-sm-9 col-xs-12" />
                 </div>
-                <button className="btn btn-lg btn-success" onClick={this.handleSubmit.bind(this)}>
-                  <i className="fa fa-sign-in" />{' '}Log In
-                  </button>
+                <div className="row bottom-margin">
+                  <label for="inputPassword" className="col-lg-3 col-md-3 col-sm-3 col-xs-12">Password</label>
+                  <input type="password" id="inputPassword" name="password" placeholder="Password"
+                    className="form-control col-lg-9 col-md-9 col-sm-9 col-xs-12" required="" />
+                </div>
+                <div className="row bottom-margin">
+                  <button className="sap-btn sap-btn-primary" onClick={this.handleSubmit.bind(this)}>
+                    <i className="fa fa-sign-in" />{' '}Log In
+                    </button>
+                </div>
               </div>
             </form>
           </div>
@@ -47,6 +55,9 @@ class App extends Component {
         <div className="col-lg-4 col-md-4 col-sm-3 col-xs-3"></div>
       </div>
       /*
+                    <input type="text" ref="username" placeholder="Enter a username"
+                      className="form-control" />
+
               <div className="container">
                 <h2>Form control: input</h2>
                 <p>The form below contains two input elements; one of type text and one of type password:</p>
